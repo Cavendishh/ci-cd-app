ffd7eb875d1e229a523610c89adf99897648d060
Cavendishh <kavanderjanne@gmail.com>
2022-01-05 21:10:37 +0200

    Fix lint error

--- client/components/App.js
+++ client/components/App.js
@@ -3,7 +3,7 @@ import NavBar from 'Components/NavBar'
 import Footer from 'Components/Footer'
 import Router from 'Components/Router'
 
-const App () => (
+const App = () => (
   <>
     <NavBar />
     <Router />
