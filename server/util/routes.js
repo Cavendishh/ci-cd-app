5a12119c85cac7f17df27de74fb18e10c5868055
Cavendishh <kavanderjanne@gmail.com>
2022-01-05 21:36:53 +0200

    Add health check

--- server/util/routes.js
+++ server/util/routes.js
@@ -3,6 +3,8 @@ const messages = require('@controllers/messagesController')
 
 const router = Router()
 
+router.get('/health', messages.getHealth)
+router.get('/messages', messages.getAll)
 router.get('/messages', messages.getAll)
 router.post('/messages', messages.create)
 router.delete('/messages/:id', messages.destroy)
